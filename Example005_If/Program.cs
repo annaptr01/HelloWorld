1cd2b961df65fdf80ab48b6d5cd6bc879eb547eb
anna <annetakaulitz@mail.ru>
2023-04-20 18:42:22 +0300

    add Example 005 If project

---
+++ Example005_If/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Insert user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "maria")
+{
+    Console.WriteLine("Yay! Welcome Maria!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
